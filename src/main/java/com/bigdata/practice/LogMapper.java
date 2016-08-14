3fe35e4e1e9fe7b76715038780cf2204aee3c202
nmupp <nmupphere@gmail.com>
2016-08-13 23:06:27 -0500

    space refactoring

--- src/main/java/com/bigdata/practice/LogMapper.java
+++ src/main/java/com/bigdata/practice/LogMapper.java
@@ -4,7 +4,6 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
-
 import java.io.IOException;
 
 /**
